66fe984f146b845810fe762c155209b830bb25b9
Jeremy Su <os369510@gmail.com>
2024-04-23 09:26:32 +0800

    [s/start-kvm.sh] enhance the script for x86 on my arch

--- scripts/start-kvm.sh
+++ scripts/start-kvm.sh
@@ -77,24 +77,60 @@ case "$OS" in
         eval $CMD
         ;;
     *)
-        PATH_UBUNTU="$HOME/Qemu/Ubuntu"
-        case $1 in
-            ubuntu)
-                qemu-system-x86_64\
-                    -m 8G\
-                    -vga std\
-                    -mem-path /dev/hugepages\
-                    -enable-kvm\
-                    -machine q35,accel=kvm\
-                    -device intel-iommu\
-                    -device vfio-pci,host=01:00.0\
-                    -cpu host\
-                    -drive format=raw,file=$PATH_UBUNTU/ubuntu-1804-disk
+        if [ "$#" -lt 2 ]; then
+            usage
+        fi
+        if [ "$2" == "install" ]; then
+            install=yes
+        fi
+        LIBVIRT_PATH="$HOME/.local/share/libvirt/"
+        BOOTLOADER="$LIBVIRT_PATH/edk2/"
+        IMG_PATH="$LIBVIRT_PATH/images"
+        case "$1" in
+            aarch64)
+                ISO="ubuntu-22.04.4-live-server-arm64.iso"
+                DISK="ubuntu-22.04-server-aarch64.qcow2"
+                ARG="-accel tcg -cpu cortex-a710 -smp 8 -M virt -m 3000 \
+                     -bios $BOOTLOADER/$1/QEMU_EFI.fd \
+                     -monitor stdio \
+                     -display default,show-cursor=on \
+                     -device qemu-xhci -device usb-kbd -device usb-tablet \
+                     -device intel-hda \
+                     -drive id=hd0,media=disk,if=none,format=qcow2,file="$IMG_PATH/$DISK" \
+                     -device virtio-blk-device,drive=hd0"
+                if [ -n "$install" ]; then
+                    ARG="$ARG \
+                     -drive id=cdrom1,media=cdrom,if=none,file="$IMG_PATH/$ISO" \
+                     -device virtio-scsi-device -device scsi-cd,drive=cdrom1"
+                fi
+                CMD="qemu-system-aarch64 $ARG"
                 ;;
             *)
-                echo "Usage: $0 [ubuntu] (options)"
-                exit 1
+                usage
                 ;;
         esac
+        echo "--- command is"
+        echo $CMD
+        echo "---"
+        eval $CMD
+#        PATH_UBUNTU="$HOME/Qemu/Ubuntu"
+#        case $1 in
+#            ubuntu)
+#                qemu-system-x86_64\
+#                    -m 8G\
+#                    -vga std\
+#                    -mem-path /dev/hugepages\
+#                    -enable-kvm\
+#                    -machine q35,accel=kvm\
+#                    -device intel-iommu\
+#                    -device vfio-pci,host=01:00.0\
+#                    -cpu host\
+#                    -drive format=raw,file=$PATH_UBUNTU/ubuntu-1804-disk
+#                ;;
+#            *)
+#                echo "Usage: $0 [ubuntu] (options)"
+#                exit 1
+#                ;;
+#        esac
         ;;
 esac
